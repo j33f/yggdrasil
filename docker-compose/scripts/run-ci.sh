2f3258d8ec8a3f82110d24bde0b1942b441c67eb
jeff <jeff@modulaweb.fr>
2021-03-15 20:02:38 +0100

    ci(func tests): try to fix

--- docker-compose/scripts/run-ci.sh
+++ docker-compose/scripts/run-ci.sh
@@ -19,28 +19,4 @@ fi
 npm install -g npm
 npm install --force --build-from-source
 
-echo "[$(date)] - Starting..."
-
-npm run test:functional
-FUNC_TESTS_EXIT_CODE=$? # store the tests exit code
-
-if [ $FUNC_TESTS_EXIT_CODE -eq 0 ]
-then
-  echo ''
-  echo ''
-  echo -e "\e[32mFunctional tests passed !\e[0m"
-  echo ''
-  echo ''
-
-  npm run unit:coverage-summary
-
-  exit 0
-else
-  echo ''
-  echo ''
-  echo -e "\e[31Functional tests failed...\e[0m"
-  echo ''
-  echo ''
-
-  exit $FUNC_TESTS_EXIT_CODE
-fi
+tail -f /dev/null
