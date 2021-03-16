6e27b4f408a4396cf93b65857ee3df56fb5e9b0d
jeff <jeff@modulaweb.fr>
2021-03-16 01:30:36 +0100

    try to fix func tests on ci

--- features/run.sh
+++ features/run.sh
@@ -5,15 +5,15 @@ cd /var/app/
 echo 'Starting the server in testing mode... (please wait)'
 
 ## Starts the server silently
-##npx nyc --temp-directory ./.nyc_output.func --report-dir coverage.functional --reporter=text-summary --reporter=lcov ./bin/startServerInTestingMode &>/dev/null &
+##./node_modules/nyc/bin/nyc.js --temp-directory ./.nyc_output.func --report-dir coverage.functional --reporter=text-summary --reporter=lcov ./bin/startServerInTestingMode &>/dev/null &
 ## Starts the server verbosely for testing
-nyc --temp-directory ./.nyc_output.func --report-dir coverage.functional --reporter=text-summary --reporter=lcov ./bin/startServerInTestingMode &
+./node_modules/nyc/bin/nyc.js --temp-directory ./.nyc_output.func --report-dir coverage.functional --reporter=text-summary --reporter=lcov ./bin/startServerInTestingMode &
 
 ## Awaiting for the server to be started
 printf "Awaiting to connect to the testing server"
 
 ATTEMPTSr=0
-MAX_ATTEMPTS=60
+MAX_ATTEMPTS=30
 
 until $(curl --output /dev/null --silent --head --fail http://localhost:8843); do
     if [ ${ATTEMPTSr} -eq ${MAX_ATTEMPTS} ];then
@@ -24,7 +24,7 @@ until $(curl --output /dev/null --silent --head --fail http://localhost:8843); d
 
     printf '.'
     ATTEMPTSr=$((ATTEMPTSr+1))
-    sleep 1
+    sleep 2
 done
 echo '';
 
@@ -36,10 +36,10 @@ TESTS_EXIT_CODE=$? # store the tests exit code
 echo 'Tests are done : shutting down the server'
 
 ## kill the server via SIGINT
-kill -2 `cat /var/run/yggdrasilTestingServer.pid`
+kill -2 `cat /var/app/yggdrasilTestingServer.pid`
 
 ## remove the created pid file
-rm /var/run/yggdrasilTestingServer.pid
+rm /var/app/yggdrasilTestingServer.pid
 
 ## exits with the tests exit code to reflect the success or failing of those tests
 exit $TESTS_EXIT_CODE
